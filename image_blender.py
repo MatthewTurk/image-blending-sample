45ff166ec3aa7a4e1417bee4a20587e5f299fc52
Matthew Turk <matthewturk@gmail.com>
2018-02-06 11:57:11 -0600

    Initial import of image_blender.py

---
+++ image_blender.py
@@ -0,0 +1,7 @@
+import astropy.io.fits as fits
+nmos502 = fits.open("502nmos.fits")
+nmos656 = fits.open("656nmos.fits")
+nmos673 = fits.open("673nmos.fits")
+v1 = nmos502[0].data
+v2 = nmos656[0].data
+v3 = nmos673[0].data
