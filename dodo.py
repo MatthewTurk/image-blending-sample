7e96b8a193a8f135eeeba432e9f24ac37d6ede98
Matthew Turk <matthewturk@gmail.com>
2018-02-08 11:46:21 -0600

    Fixing dodo.py to use run_once

--- dodo.py
+++ dodo.py
@@ -1,13 +1,15 @@
+from doit.tools import run_once
+
 filenames = ['502nmos.zip', '656nmos.zip', '673nmos.zip']
 base_url = "https://www.spacetelescope.org/static/projects/fits_liberator/datasets/eagle/"
 
 def task_download():
   actions = []
   for filename in filenames:
-    actions.append(['wget', '--no-clobber',
-                    base_url + filename])
+    actions.append(['wget', base_url + filename])
   return {'actions': actions,
-          'targets': filenames }
+          'targets': filenames,
+          'uptodate': [run_once]}
 
 def task_unzip():
   actions = []
@@ -17,5 +19,8 @@ def task_unzip():
     actions.append( ['unzip', '-o', filename] )
     file_dep.append(filename)
     targets.append( filename.replace('.zip', '.fits') )
-  return {'actions': actions, 'file_dep': 
-file_dep, 'targets': targets}
+  return {'actions': actions,
+          'file_dep': file_dep,
+          'targets': targets}
+
+
